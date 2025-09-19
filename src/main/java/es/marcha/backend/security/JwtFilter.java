c970c8e8b36c2d87361da04c8be8ce3966fa5272
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-19 21:32:57 +0200

    Add JWT filter and utility classes for token validation and generation

---
+++ src/main/java/es/marcha/backend/security/JwtFilter.java
@@ -0,0 +1,48 @@
+package es.marcha.backend.security;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+     @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+
+            try {
+                // validar token y sacar usuario
+                String user = JwtUtil.validarToken(token);
+                System.out.println("✅ Token válido para usuario: " + user);
+
+                // crear Authentication sin roles
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(user, null, null);
+
+                // meterlo en el contexto de Spring
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido o expirado");
+                return;
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ src/main/java/es/marcha/backend/security/JwtUtil.java
@@ -0,0 +1,41 @@
+package es.marcha.backend.security;
+
+import java.util.Base64;
+import java.util.Date;
+import javax.crypto.SecretKey;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+    private static  SecretKey SECRET_KEY;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        System.out.println("SECRET: "+secret);
+        SECRET_KEY = Keys.hmacShaKeyFor(Base64.getDecoder().decode(secret));
+
+    }
+       
+
+    public static String generarToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public static String validarToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+    
+}
