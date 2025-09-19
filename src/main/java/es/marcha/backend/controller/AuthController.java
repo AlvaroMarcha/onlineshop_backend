51e25672cc0ee547a563a0b46bc309483e1be4f6
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-19 22:38:02 +0200

    Refactor login endpoint to real login

--- src/main/java/es/marcha/backend/controller/AuthController.java
+++ src/main/java/es/marcha/backend/controller/AuthController.java
@@ -1,28 +1,25 @@
 package es.marcha.backend.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import es.marcha.backend.model.User;
 import es.marcha.backend.security.JwtUtil;
+import es.marcha.backend.services.ClientService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    //Attribs
+    @Autowired
+    private ClientService clientService;
 
-    	//TESTING...
+    //TESTING...
 	@PostMapping("/login")
     public String login(@RequestParam String user, @RequestParam String pass) {
-		System.out.println("LOGIN - Usuario: " + user + " - Pass: " + pass);
-        // Aquí validarías contra BD o lo que uses
-        if ("alan".equals(user) && "1234".equals(pass)) {
-            return JwtUtil.generarToken(user);
-        } else {
-            throw new RuntimeException("Credenciales inválidas");
-        }
+        User existUser = clientService.getClientByUsername(user);
+        return existUser !=null && existUser.getPassword().equals(pass) ? JwtUtil.generarToken(user) : null;
     }
-
-   
-    
 }
