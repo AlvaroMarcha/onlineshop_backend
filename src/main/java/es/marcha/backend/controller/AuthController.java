cd2b065ae393bf3ff9acc2f98101eb65d7736c21
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-23 22:20:07 +0200

    Add Cors config to project to connect with Angular frontend

--- src/main/java/es/marcha/backend/controller/AuthController.java
+++ src/main/java/es/marcha/backend/controller/AuthController.java
@@ -3,15 +3,18 @@ package es.marcha.backend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import es.marcha.backend.dto.request.LoginRequestDTO;
 import es.marcha.backend.dto.response.ResponseAuthDTO;
 import es.marcha.backend.model.User;
 import es.marcha.backend.security.JwtUtil;
 import es.marcha.backend.services.ClientService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
@@ -21,11 +24,11 @@ public class AuthController {
 
     //TESTING...
 	@PostMapping("/login")
-    public ResponseEntity<ResponseAuthDTO> login(@RequestParam String user, @RequestParam String pass) {
-        User existUser = clientService.getClientByUsername(user);
+    public ResponseEntity<ResponseAuthDTO> login(@RequestBody LoginRequestDTO loginRequest){ 
+        User existUser = clientService.getClientByUsername(loginRequest.getUser());
         String token = null;
-        if (existUser != null && existUser.getPassword().equals(pass)){
-            token = JwtUtil.generarToken(user);
+        if (existUser != null && existUser.getPassword().equals(loginRequest.getPass())){
+            token = JwtUtil.generarToken(loginRequest.getUser());
         }else{
             return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
