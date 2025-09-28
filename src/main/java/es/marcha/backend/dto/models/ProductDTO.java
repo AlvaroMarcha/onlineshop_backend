dc275e5aa15ba29c110566d652ef8855c1b71bbb
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-28 16:55:50 +0200

    Add ProductDTO class for product data transfer

---
+++ src/main/java/es/marcha/backend/dto/models/ProductDTO.java
@@ -0,0 +1,23 @@
+package es.marcha.backend.dto.models;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDTO {
+    private Long id;
+    private String name;
+    private Integer stock;
+    private BigDecimal price;
+    private Boolean visible;
+    private String category;
+    private String subcategory;
+
+    
+}
