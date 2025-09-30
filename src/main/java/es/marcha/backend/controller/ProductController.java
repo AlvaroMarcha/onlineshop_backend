293c7f68ed6840dd482172286cea2dc5ba9b9822
AlvaroMarcha <alvarochavas1994@gmail.com>
2025-09-30 21:53:40 +0200

    Add CrossOrigin to controller at localhost4200

--- src/main/java/es/marcha/backend/controller/ProductController.java
+++ src/main/java/es/marcha/backend/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import es.marcha.backend.dto.models.ProductDTO;
 import es.marcha.backend.services.inventory.ProductService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("backoffice/")
 public class ProductController {
