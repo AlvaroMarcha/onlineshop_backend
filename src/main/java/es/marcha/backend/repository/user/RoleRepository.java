425a9b6c8f4e19821f7c19fb1ce0b2c693769dac
AlvaroMarcha <alvarochavas1994@gmail.com>
2026-02-01 02:06:05 +0100

    Move RoleRepository and UserRepository in repository/user

--- src/main/java/es/marcha/backend/repository/user/RoleRepository.java
+++ src/main/java/es/marcha/backend/repository/user/RoleRepository.java
@@ -1,9 +1,8 @@
-package es.marcha.backend.repository;
+package es.marcha.backend.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import es.marcha.backend.model.Role;
+import es.marcha.backend.model.user.Role;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
--- src/main/java/es/marcha/backend/repository/user/UserRepository.java
+++ src/main/java/es/marcha/backend/repository/user/UserRepository.java
@@ -1,10 +1,11 @@
-package es.marcha.backend.repository;
+package es.marcha.backend.repository.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import es.marcha.backend.model.User;
+import es.marcha.backend.model.user.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
+
