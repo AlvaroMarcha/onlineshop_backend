bd41713a3f29437f10f02ef93797af7fba1b595d
AlvaroMarcha <alvarochavas1994@gmail.com>
2026-02-02 16:34:36 +0100

    Add new static variables in UserException.class

--- src/main/java/es/marcha/backend/exception/UserException.java
+++ src/main/java/es/marcha/backend/exception/UserException.java
@@ -7,6 +7,10 @@ public class UserException extends NoHandlerException {
     public static final String FAILED_SAVE = "FAILED_TO_SAVE_USER";
     public static final String FAILED_UPDATE = "FAILED_TO_UPDATE_USER";
     public static final String FAILED_DELETE = "FAILED_TO_DELETE_USER";
+    public static final String FAILED_LOGIN = "FAILED_TO_LOGIN";
+    public static final String FAILED_REGISTER = "FAILED_TO_REGISTER";
+    public static final String FAILED_CREATE_USER = "USER_ALREADY_EXIST";
+    public static final String TOKEN_FAILED = "TOKEN_NOT_ALLOWED";
 
     public UserException() {
         this(DEFAULT);
