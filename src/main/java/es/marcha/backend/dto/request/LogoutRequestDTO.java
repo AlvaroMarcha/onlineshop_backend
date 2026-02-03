92e379643f331b59dec594f37b256d74a9a059df
AlvaroMarcha <alvarochavas1994@gmail.com>
2026-02-03 22:55:38 +0100

    Add logout endpoint and DTOs for logout request and response

---
+++ src/main/java/es/marcha/backend/dto/request/LogoutRequestDTO.java
@@ -0,0 +1,16 @@
+package es.marcha.backend.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class LogoutRequestDTO {
+    private long userId;
+}
