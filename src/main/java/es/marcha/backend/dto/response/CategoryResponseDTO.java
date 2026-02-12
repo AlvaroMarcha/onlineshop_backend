2a74048f6f0f807a4847228397ae1355aa775d3b
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-12 18:42:12 +0100

    Add CategoryResponseDTO and CategoryMapper for category data handling

---
+++ src/main/java/es/marcha/backend/dto/response/CategoryResponseDTO.java
@@ -0,0 +1,19 @@
+package es.marcha.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class CategoryResponseDTO {
+    private long id;
+    private String name;
+    private String description;
+    private String slug;
+}
--- src/main/java/es/marcha/backend/dto/response/ProductResponseDTO.java
+++ src/main/java/es/marcha/backend/dto/response/ProductResponseDTO.java
@@ -1,7 +1,6 @@
 package es.marcha.backend.dto.response;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.List;
 
 import es.marcha.backend.model.ecommerce.Subcategory;
