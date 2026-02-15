7682565707c7a5958363e428f8b1aa6d37bf37d2
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-16 00:53:13 +0100

    Implement basic CRUD operations for Subcategory with DTO mapping

---
+++ src/main/java/es/marcha/backend/dto/response/SubcategoryResponseDTO.java
@@ -0,0 +1,20 @@
+package es.marcha.backend.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class SubcategoryResponseDTO {
+    private long id;
+    private String name;
+    private String description;
+    private String slug;
+
+}
---
+++ src/main/java/es/marcha/backend/mapper/SubcategoryMapper.java
@@ -0,0 +1,23 @@
+package es.marcha.backend.mapper;
+
+import es.marcha.backend.dto.response.SubcategoryResponseDTO;
+import es.marcha.backend.model.ecommerce.Subcategory;
+
+public class SubcategoryMapper {
+    public static SubcategoryResponseDTO toResponseDTO(Subcategory subcategory) {
+        return SubcategoryResponseDTO.builder()
+                .id(subcategory.getId())
+                .name(subcategory.getName())
+                .description(subcategory.getDescription())
+                .slug(subcategory.getSlug())
+                .build();
+    }
+
+    public static Subcategory toSubcategory(SubcategoryResponseDTO dto, Subcategory subcategory) {
+        subcategory.setName(dto.getName());
+        subcategory.setDescription(dto.getDescription());
+        subcategory.setSlug(dto.getSlug());
+        return subcategory;
+    }
+
+}
