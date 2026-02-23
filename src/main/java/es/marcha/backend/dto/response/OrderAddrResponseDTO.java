c46c0d83f57493fab3d981cdd8a3a6e87d8ba95a
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-23 11:10:54 +0100

    Add OrderAddresses model and OrderAddrResponseDTO with mapper

---
+++ src/main/java/es/marcha/backend/dto/response/OrderAddrResponseDTO.java
@@ -0,0 +1,30 @@
+package es.marcha.backend.dto.response;
+
+import java.time.LocalDateTime;
+
+import es.marcha.backend.model.enums.AddressesType;
+import es.marcha.backend.model.order.Order;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class OrderAddrResponseDTO {
+    private long id;
+    private Order order;
+    private AddressesType type;
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private String postalCode;
+    private String country;
+    private LocalDateTime createdAt;
+
+}
