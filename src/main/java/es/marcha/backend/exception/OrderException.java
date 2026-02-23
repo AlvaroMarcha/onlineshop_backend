6d57f80693c0b757ab13b770400b86b05ba44bb0
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-23 11:53:48 +0100

    Add OrderAddressService and OrderAddrRepository with exception handling for order addresses

--- src/main/java/es/marcha/backend/exception/OrderException.java
+++ src/main/java/es/marcha/backend/exception/OrderException.java
@@ -7,6 +7,8 @@ public class OrderException extends NoHandlerException {
     public static final String NOT_VALID_PAYMENT = "NOT_VALID_PAYMENT FOUNDS";
     public static final String TERMINAL_STATUS_PAYMENT = "PAYMENT_HAS_A_FINAL_STATUS";
     public static final String DUPLICATE_TRANSACTION = "TRANSACTION_ID_DUPLICATED";
+    public static final String FAILED_ORDER_ADDRESSES = "FAILED_TO_FETCH_ORDER_ADDRESS";
+    public static final String FAILED_ORDER_ADDRESS = "ORDER_ADDRESS_NOT_FOUND";
 
     public OrderException() {
         this(DEFAULT);
