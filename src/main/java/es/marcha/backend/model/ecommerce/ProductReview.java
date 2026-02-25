4e5249c7d5591fe0dbdaf2aeac4cb1ae013d733f
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-25 23:45:04 +0100

    ProductReview entity and Mapper.

---
+++ src/main/java/es/marcha/backend/model/ecommerce/ProductReview.java
@@ -0,0 +1,59 @@
+package es.marcha.backend.model.ecommerce;
+
+import java.time.LocalDateTime;
+
+import es.marcha.backend.model.user.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+@Entity
+@Table(name = "product_reviews", uniqueConstraints = {
+        @UniqueConstraint(name = "unique_user_product_review", columnNames = { "user_id", "product_id" })
+})
+public class ProductReview {
+    // Attribs
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+    @Column(name = "rating", nullable = false)
+    private int rating;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "comment", nullable = false)
+    private String comment;
+    @Column(name = "likes")
+    private int likes;
+    @Column(name = "dislikes")
+    private int dislikes;
+    @Column(name = "is_active")
+    private boolean isActive;
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
