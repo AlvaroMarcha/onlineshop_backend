da342cef214ae82675d04ef143ff02cd201ac4cf
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-26 13:51:27 +0100

    Add ProductReviewUserResponseDTO and update ProductReviewResponseDTO to use it

---
+++ src/main/java/es/marcha/backend/dto/response/ecommerce/ProductReviewUserReponseDTO.java
@@ -0,0 +1,19 @@
+package es.marcha.backend.dto.response.ecommerce;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class ProductReviewUserReponseDTO {
+    // Attribs
+    private long userId;
+    private String name;
+    private String surname;
+}
