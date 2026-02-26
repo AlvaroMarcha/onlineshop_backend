cf1342337dd20b9a91487417e1e479975a476da9
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-26 14:54:05 +0100

    Remove custom query annotation from findAllByProductId method in ProductReviewRepository

--- src/main/java/es/marcha/backend/repository/ecommerce/ProductReviewRepository.java
+++ src/main/java/es/marcha/backend/repository/ecommerce/ProductReviewRepository.java
@@ -3,8 +3,6 @@ package es.marcha.backend.repository.ecommerce;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import es.marcha.backend.model.ecommerce.ProductReview;
@@ -12,6 +10,5 @@ import es.marcha.backend.model.ecommerce.ProductReview;
 @Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
 
-    @Query("SELECT r FROM ProductReview r WHERE r.product.id = :productId")
-    List<ProductReview> findAllByProductId(@Param("productId") long productId);
+    List<ProductReview> findAllByProductId(long productId);
 }
