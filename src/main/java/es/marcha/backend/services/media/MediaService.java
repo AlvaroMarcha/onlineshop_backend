e612b684dc7c955679a784d007b201c9fb94e370
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-27 14:36:37 +0100

    Remove unused import statement for Autowired in MediaService

--- src/main/java/es/marcha/backend/services/media/MediaService.java
+++ src/main/java/es/marcha/backend/services/media/MediaService.java
@@ -7,7 +7,6 @@ import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Set;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
