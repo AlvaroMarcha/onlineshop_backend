747afaf26ccf4134dd82876a23c3e5c663d9baf5
AlanMarcha <alvarochavas1994@gmail.com>
2026-02-28 17:03:50 +0100

    Add ProductAttribRequestDTO and ProductAttribValueRequestDTO classes for product attribute requests

---
+++ src/main/java/es/marcha/backend/dto/request/ecommerce/product/ProductAttribRequestDTO.java
@@ -0,0 +1,22 @@
+package es.marcha.backend.dto.request.ecommerce.product;
+
+import es.marcha.backend.model.enums.AttribType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Setter
+@Getter
+public class ProductAttribRequestDTO {
+    private String name;
+    private String description;
+    private String slug;
+    private AttribType type;
+    private boolean isRequired;
+    private int sortOrder;
+}
